f9cd0100766d0a337e366b7587059dc1953677c3
VBeatDead <77430117+VBeatDead@users.noreply.github.com>
2023-10-23 20:34:57 +0700

    Update Task

---
+++ Modul3/task/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <h2>Selamat Datang di Home Page</h2>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
