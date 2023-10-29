fe114dd1cf103674c52d081d8824c2a43b4a862e
VBeatDead <77430117+VBeatDead@users.noreply.github.com>
2023-10-30 05:56:40 +0700

    Edit Nav

--- Modul3/prak/src/components/Navbar.jsx
+++ Modul3/prak/src/components/Navbar.jsx
@@ -15,15 +15,9 @@ const Navbar = () => {
           </div>
           <div className="col-6 col-lg-8 d-flex justify-content-center">
             <div className="navbar-nav d-flex align-items-center">
-              <Link to="/" className="nav-link text-center" style={{ marginRight: '5px' }}>
-                HOME
-              </Link>
-              <Link to="/about" className="nav-link text-center" style={{ marginRight: '5px' }}>
-                ABOUT US
-              </Link>
-              <Link to="/contact" className="nav-link text-center" style={{ marginRight: '5px' }}>
-                CONTACT
-              </Link>
+              <Link to="/" className="nav-link text-center" style={{ marginRight: '5px' }}>HOME</Link>
+              <Link to="/about" className="nav-link text-center" style={{ marginRight: '5px' }}>ABOUT US</Link>
+              <Link to="/contact" className="nav-link text-center" style={{ marginRight: '5px' }}>CONTACT</Link>
             </div>
           </div>
           <div className="col-12 col-lg-2 d-flex align-items-center justify-content-center">
